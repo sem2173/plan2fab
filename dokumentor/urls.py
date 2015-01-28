31da8d6d8a00c12d7c9272a59c2439889fa4fb21
Sem <semia.ladjal@gmail.com>
2015-01-28 18:02:14 +0100

    Can view project

--- dokumentor/urls.py
+++ dokumentor/urls.py
@@ -4,6 +4,7 @@ from dokumentor import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
+        url(r'^projects/(\d+)/$', views.view_project),
         url(r'name_step', views.name_step, name='name_step'),
         url(r'build_step', views.build_step, name='build_step'),
         )
--- dokumentor/views.py
+++ dokumentor/views.py
@@ -22,3 +22,7 @@ def name_step(request):
 def build_step(request):
     form = BuildStepForm()
     return render(request, 'dokumentor/build_step.html', {'form': form})
+
+def view_project(request, id):
+    project = Project.objects.get(id=id)
+    return render(request, 'dokumentor/project.html', {'project': project})
