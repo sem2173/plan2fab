c1e90359b0d061045ec104ddf0e06ccc86f06ac6
giniouxe <aurelie.ginioux@gmail.com>
2015-03-04 13:19:58 +0100

    Test when project id does not exist

--- dokumentor/tests.py
+++ dokumentor/tests.py
@@ -5,16 +5,21 @@ from dokumentor.models import Project
 
 class ProjectTests(TestCase):
 
-    def test_was_published_recently_with_future_question(self):
+    def test_project_as_name(self):
         Project.objects.create(name="SomeThing", duration=0)
         project = Project.objects.get(name="SomeThing")
         self.assertEqual(project.name, "SomeThing")
 
     def test_form_to_create_project(self):
-        response = self.client.post(reverse('projects:name_step'), {'name': "A Project name", 'description': 'a beautiful project I started...'})
+        response = self.client.post(reverse('projects:name_step'),
+            {'name': "A Project name", 'description': 'a beautiful project I started...'})
 
         projects = Project.objects.order_by('name')
         project = Project.objects.get(name="A Project name")
         self.assertEqual("A Project name", project.name)
         self.assertContains(response, '', status_code=302)
 
+    def test_no_build_form_when_project_doesnt_exist(self):
+        response = self.client.get(reverse('projects:build_step', args=[42]))
+
+        self.assertRedirects(response, reverse('projects:index'), status_code=302, target_status_code=200)
